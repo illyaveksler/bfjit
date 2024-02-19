e78a502aff3829b80c05a246a577842e421203bd
Aurus <55865051+AAurus@users.noreply.github.com>
2024-02-19 01:56:06 -0800

    completed backpatching and translation unit for bfjit

--- skullfuck.hs
+++ skullfuck.hs
@@ -24,8 +24,9 @@ charToBFC c
     | (c == ']') = LoopEnd
     | otherwise = BFInvalid
 
--- first element of tuple is the ML string, second is byte length
-bfcTupleToML :: (BFCommand, Int) -> (String, Int)
+-- first element of tuple is the ML string, second is byte length, third is backpatch
+--                                                                    index indicator
+bfcTupleToML :: (BFCommand, Int) -> (String, Int, Int)
 bfcTupleToML tuple
     | (fst tuple == IncrPtr) = incrPtrToML (snd tuple)
     | (fst tuple == DecrPtr) = decrPtrToML (snd tuple)
@@ -33,52 +34,64 @@ bfcTupleToML tuple
     | (fst tuple == DecrVal) = decrValToML (snd tuple)
     | (fst tuple == OutChar) = outCharToML (snd tuple)
     | (fst tuple == InpChar) = inpCharToML (snd tuple)
-    | (fst tuple == LoopStt) = ("TODO: LOOPSTT",0)
-    | (fst tuple == LoopEnd) = ("TODO: LOOPEND",0)
-    | otherwise = ("UNREACHABLE",0)
+    | (fst tuple == LoopStt) = loopSttToML (snd tuple)
+    | (fst tuple == LoopEnd) = loopEndToML (snd tuple)
+    | otherwise = ("UNREACHABLE",0,-1)
 
 -- Machine Language Translation adapted from bfjit by tsoding
 -- https://github.com/tsoding/bfjit
 -- ( https://github.com/tsoding/bfjit/blob/main/bfjit.c )
 
-incrValToML count = ("\x80\x07"++[chr (count .&. 255)],3)
+incrValToML count = ("\x80\x07"++[chr (count .&. 255)],3, -1)
 
-decrValToML count = ("\x80\x2F"++[chr (count .&. 255)],3)
+decrValToML count = ("\x80\x2F"++[chr (count .&. 255)],3, -1)
 
-incrPtrToML count = ("\x48\x81\xC7"++(intToUInt32Bytes count),7)
+incrPtrToML count = ("\x48\x81\xC7"++(intToUInt32Bytes count),7, -1)
 
-decrPtrToML count = ("\x48\x81\xEF"++(intToUInt32Bytes count),7)
+decrPtrToML count = ("\x48\x81\xEF"++(intToUInt32Bytes count),7, -1)
 
-inpCharToML count = (callMultiple ("",0) count inputCall)
+inpCharToML count = (callMultiple ("",0,-1) count inputCall)
 
-outCharToML count = (callMultiple ("",0) count outputCall)
+outCharToML count = (callMultiple ("",0,-1) count outputCall)
+
+loopSttToML index = ((fst loopStartCall), (snd loopStartCall), index)
+
+loopEndToML index = ((fst loopStartCall), (snd loopEndCall), index)
 
 
 
-
-
-callMultiple :: (String, Int) -> Int -> (String, Int) -> (String, Int)
+callMultiple :: (String, Int, Int) -> Int -> (String, Int) -> (String, Int, Int)
 callMultiple result 0 input = result
 callMultiple result count input = 
-                callMultiple (((fst result) ++ (fst input)), ((snd result)+(snd input))) (count-1) input
+                callMultiple (((fst_3 result) ++ (fst input)), ((snd_3 result)+(snd input)), trd_3 result) (count-1) input
 
 inputCall = (("\x57" ++
-              "\x48\xc7\xc0\x01\x00\x00\x00" ++
-              "\x48\x89\xfe" ++
-              "\x48\xc7\xc7\x01\x00\x00\x00" ++
-              "\x48\xc7\xc2\x01\x00\x00\x00" ++
-              "\x0f\x05" ++
-              "\x5f"), 
+              "\x48\xC7\xC0\x01\x00\x00\x00" ++
+              "\x48\x89\xFE" ++
+              "\x48\xC7\xC7\x01\x00\x00\x00" ++
+              "\x48\xC7\xC2\x01\x00\x00\x00" ++
+              "\x0F\x05" ++
+              "\x5F"), 
              28)
 
 outputCall = (("\x57" ++
-               "\x48\xc7\xc0\x00\x00\x00\x00" ++
-               "\x48\x89\xfe" ++
-               "\x48\xc7\xc7\x00\x00\x00\x00" ++
-               "\x48\xc7\xc2\x01\x00\x00\x00" ++
-               "\x0f\x05" ++
-               "\x5f"),
+               "\x48\xC7\xC0\x00\x00\x00\x00" ++
+               "\x48\x89\xFE" ++
+               "\x48\xC7\xC7\x00\x00\x00\x00" ++
+               "\x48\xC7\xC2\x01\x00\x00\x00" ++
+               "\x0F\x05" ++
+               "\x5F"),
               28)
+              
+loopStartCall = (("\x8A\x07" ++
+                  "\x84\xC0" ++
+                  "\x0F\x84"),
+                 10)
+                 
+loopEndCall = (("\x8A\x07" ++
+                "\x84\xC0" ++
+                "\x0F\x85"),
+               10)
 
 intToUInt32Bytes n = [
     (chr (n .&. 255)),
@@ -89,48 +102,109 @@ intToUInt32Bytes n = [
 
 
 
-
-
-readBrainfuck [] = []
-readBrainfuck str = reverse (readBrainfuckHelper (brainfuckOnly str) [] [])
+brainfuckToTuples [] = []
+brainfuckToTuples str = reverse (brainfuckToTuplesHelper (brainfuckOnly str) [] [])
 
 brainfuckOnly str = foldr (\x y -> if (elem x "<>+-.,[]") then x:y else y) [] str
 
-readBrainfuckHelper [] out [] = out         -- FINISHED READING
-readBrainfuckHelper [] out (hs:ts) = []     -- ERROR: MISMATCHED LOOPS
+brainfuckToTuplesHelper [] out [] = out         -- FINISHED READING
+brainfuckToTuplesHelper [] out (hs:ts) = []     -- ERROR: MISMATCHED LOOPS
 
-readBrainfuckHelper (hi:ti) [] []
-    | (charToBFC hi) == BFInvalid = readBrainfuckHelper ti 
+brainfuckToTuplesHelper (hi:ti) [] []
+    | (charToBFC hi) == BFInvalid = brainfuckToTuplesHelper ti 
                                                         []  
                                                         [] -- SKIP INVALID
-    | ((charToBFC hi) == LoopStt) = readBrainfuckHelper ti 
-                                                        [((charToBFC hi),0)]
+    | ((charToBFC hi) == LoopStt) = brainfuckToTuplesHelper ti 
+                                                        [((charToBFC hi),0)]         -- TUPLE REPLACED BY CLOSER
                                                         [0]
     | ((charToBFC hi) == LoopEnd) = []      -- ERROR: MISMATCHED LOOPS
-    | otherwise = readBrainfuckHelper ti [((charToBFC hi),1)] []
+    | otherwise = brainfuckToTuplesHelper ti [((charToBFC hi),1)] []
 
-readBrainfuckHelper (hi:ti) (hr:tr) []
-    | (charToBFC hi) == BFInvalid = readBrainfuckHelper ti 
+brainfuckToTuplesHelper (hi:ti) (hr:tr) []
+    | (charToBFC hi) == BFInvalid = brainfuckToTuplesHelper ti 
                                                         (hr:tr)
                                                         [] -- SKIP INVALID
-    | ((charToBFC hi) == LoopStt) = readBrainfuckHelper ti 
-                                                        (((charToBFC hi),(length (hr:tr))):(hr:tr))
+    | ((charToBFC hi) == LoopStt) = brainfuckToTuplesHelper ti 
+                                                        (((charToBFC hi),0):(hr:tr)) -- TUPLE REPLACED BY CLOSER
                                                         [(length (hr:tr))]
     | ((charToBFC hi) == LoopEnd) = []      -- ERROR: MISMATCHED LOOPS
     | otherwise = if (fst hr) == (charToBFC hi) 
-                    then readBrainfuckHelper ti (((fst hr),((snd hr)+1)):tr) [] 
-                    else readBrainfuckHelper ti (((charToBFC hi),1):(hr:tr)) []
+                    then brainfuckToTuplesHelper ti (((fst hr),((snd hr)+1)):tr) [] 
+                    else brainfuckToTuplesHelper ti (((charToBFC hi),1):(hr:tr)) []
 
-readBrainfuckHelper (hi:ti) (hr:tr) (hs:ts)
-    | (charToBFC hi) == BFInvalid = readBrainfuckHelper ti 
+brainfuckToTuplesHelper (hi:ti) (hr:tr) (hs:ts)
+    | (charToBFC hi) == BFInvalid = brainfuckToTuplesHelper ti 
                                                         (hr:tr)
                                                         (hs:ts) -- SKIP INVALID
-    | ((charToBFC hi) == LoopStt) = readBrainfuckHelper ti 
-                                                        (((charToBFC hi),(length (hr:tr))):(hr:tr))
+    | ((charToBFC hi) == LoopStt) = brainfuckToTuplesHelper ti 
+                                                        (((charToBFC hi),0):(hr:tr)) -- TUPLE REPLACED BY CLOSER
                                                         ((length (hr:tr)):(hs:ts))
-    | ((charToBFC hi) == LoopEnd) = readBrainfuckHelper ti 
-                                                        (((charToBFC hi),hs):(hr:tr))
+    | ((charToBFC hi) == LoopEnd) = brainfuckToTuplesHelper ti 
+                                                        (((charToBFC hi),hs):
+                                                            (bfTuplesReplaceAt (hr:tr) hs (LoopStt, (length (hr:tr)))))
                                                         (ts)
     | otherwise = if (fst hr) == (charToBFC hi) 
-                    then readBrainfuckHelper ti (((fst hr),((snd hr)+1)):tr) (hs:ts)
-                    else readBrainfuckHelper ti (((charToBFC hi),1):(hr:tr)) (hs:ts)
+                    then brainfuckToTuplesHelper ti (((fst hr),((snd hr)+1)):tr) (hs:ts)
+                    else brainfuckToTuplesHelper ti (((charToBFC hi),1):(hr:tr)) (hs:ts)
+
+
+
+bfTuplesReplaceAt wipList pos val = listReplaceHelper wipList ((length wipList)-1-pos) val
+
+
+
+-- replaces a single element of a list at the given position.
+listReplaceHelper :: [a] -> Int -> a -> [a]
+listReplaceHelper [] _ _ = []
+listReplaceHelper (hs:ts) pos val
+    | pos < 0  = (hs:ts)
+    | pos == 0 = (val:ts)
+    | otherwise = hs:(listReplaceHelper ts (pos-1) val)
+
+fst_3 (x, _, _) = x
+snd_3 (_, x, _) = x
+trd_3 (_, _, x) = x
+
+
+
+-- applies a function to a single element of a list at the given position.
+listApplyAtHelper :: [a] -> Int -> (a -> a) -> [a]
+listApplyAtHelper [] _ _ = []
+listApplyAtHelper (hs:ts) pos f
+    | pos < 0  = (hs:ts)
+    | pos == 0 = ((f hs):ts)
+    | otherwise = hs:(listApplyAtHelper ts (pos-1) f)
+
+
+
+bfTuplesToString [] = ([],[],[])
+bfTuplesToString lst = bfTuplesToStringsHelper (map bfcTupleToML lst) 0 0 ([],[],[])
+
+
+
+bfApplyBackpatchHelper ([], [], []) = ([], [], [])
+bfApplyBackpatchHelper (code, addrs, []) = (code, addrs, [])
+bfApplyBackpatchHelper (code, addrs, (hp:tp)) = 
+    bfApplyBackpatchHelper (
+                            (listApplyAtHelper code (trd_3 hp) 
+                                               (\x -> x ++ (intToUInt32Bytes ((addrs!!(snd_3 hp)) - (fst_3 hp))))),
+                            addrs,
+                            tp
+                            )
+
+
+
+
+
+
+
+bfTuplesToStringsHelper [] _ _ result = result
+bfTuplesToStringsHelper (ht:tt) cpos cindex (code, addrs, backpatches)
+    | ((trd_3 ht) < 0) = bfTuplesToStringsHelper tt (cpos + (snd_3 ht)) (cindex+1) 
+                                                 ((code ++ [(fst_3 ht)]),
+                                                  (addrs ++ [cpos]),
+                                                  backpatches)
+    | otherwise = bfTuplesToStringsHelper tt (cpos + (snd_3 ht)) (cindex+1)
+                                          ((code ++ [fst_3 ht]),
+                                           (addrs ++ [cpos]),
+                                           (((cpos + (snd_3 ht)),(trd_3 ht),cindex):backpatches))
