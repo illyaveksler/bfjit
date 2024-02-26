ead31a5d8dc43899a3168bdbff8b2c7c54127367
Aurus <55865051+AAurus@users.noreply.github.com>
2024-02-25 17:31:33 -0800

    added error message for no usable code (mismatched brackets, etc)

--- app/MachineCode.hs
+++ app/MachineCode.hs
@@ -21,43 +21,46 @@ foreign import ccall unsafe "sys/mman.h munmap"
 
 executeMachineCode :: String -> IO ()
 executeMachineCode code = do
-    let memSize = length code
-    codeMPtr <- mmap nullPtr (fromIntegral memSize) (protRead .|. protExec .|. protWrite) (mapPrivate .|. mapAnon) (-1) 0
-    if codeMPtr == nullPtr
-        then putStrLn "Failed to allocate memory"
+    if code == "\xC3"
+        then putStrLn "No valid brainfuck code found."
         else do
-            putStrLn "Memory allocated successfully"
-            -- Fill memory with some executable code
-            forM_ (zip [0..] code) $ \(i, c) -> do
-                pokeArray (codeMPtr `plusPtr` i) [c]
-            putStrLn "Memory copied successfully"
-            -- Allocate working memory too
-            wmemPtr <- mmap nullPtr (fromIntegral jitMemoryCap) (protRead .|. protExec .|. protWrite) (mapPrivate .|. mapAnon) (-1) 0
-            if wmemPtr == nullPtr
-                then putStrLn "Failed to allocate working memory"
-                else do
-                    putStrLn "Successfully allocated working memory"
-                    putStrLn "Making sure working memory is clear..."
-                    forM_ [0..jitMemoryCap] $ \i -> do
-                        pokeArray (wmemPtr `plusPtr` i) ['\x00']
-                    putStrLn "Done!"
-                    -- Cast memory pointer to a function pointer and call the function
-                    let funPtr = castPtrToFunPtr codeMPtr
-                    putStrLn "Running!"
-                    result <- runCode funPtr wmemPtr -- MODIFY HERE: tsoding also passed in a section of allocated memory for the "tape" that the pointer moves along and modifies (essentially the only difference)
-                                                     -- this worked for tsoding because the argument passed in was stored in rdi on compile; by passing in a pointer to alloc'd memory, the brainfuck pointer was set
-                                                     -- and ready
-                    putStrLn $ "\nFunction returned: " ++ show result
-                    -- Clean up
-                    res_unmap_wmemPtr <- munmap wmemPtr (fromIntegral jitMemoryCap)
-                    if res_unmap_wmemPtr /= 0
-                        then putStrLn "Failed to unmap working memory"
-                        else do 
-                            putStrLn "Working memory unmapped successfully"
-                            result_unmap <- munmap codeMPtr (fromIntegral memSize)
-                            if result_unmap /= 0
-                                then putStrLn "Failed to unmap code memory"
-                                else putStrLn "Code memory unmapped successfully"
+        let memSize = length code
+        codeMPtr <- mmap nullPtr (fromIntegral memSize) (protRead .|. protExec .|. protWrite) (mapPrivate .|. mapAnon) (-1) 0
+        if codeMPtr == nullPtr
+            then putStrLn "Failed to allocate memory"
+            else do
+                putStrLn "Memory allocated successfully"
+                -- Fill memory with some executable code
+                forM_ (zip [0..] code) $ \(i, c) -> do
+                    pokeArray (codeMPtr `plusPtr` i) [c]
+                putStrLn "Memory copied successfully"
+                -- Allocate working memory too
+                wmemPtr <- mmap nullPtr (fromIntegral jitMemoryCap) (protRead .|. protExec .|. protWrite) (mapPrivate .|. mapAnon) (-1) 0
+                if wmemPtr == nullPtr
+                    then putStrLn "Failed to allocate working memory"
+                    else do
+                        putStrLn "Successfully allocated working memory"
+                        putStrLn "Making sure working memory is clear..."
+                        forM_ [0..jitMemoryCap] $ \i -> do
+                            pokeArray (wmemPtr `plusPtr` i) ['\x00']
+                        putStrLn "Done!"
+                        -- Cast memory pointer to a function pointer and call the function
+                        let funPtr = castPtrToFunPtr codeMPtr
+                        putStrLn "Running..."
+                        result <- runCode funPtr wmemPtr -- MODIFY HERE: tsoding also passed in a section of allocated memory for the "tape" that the pointer moves along and modifies (essentially the only difference)
+                                                        -- this worked for tsoding because the argument passed in was stored in rdi on compile; by passing in a pointer to alloc'd memory, the brainfuck pointer was set
+                                                        -- and ready
+                        putStrLn $ "\nFunction returned: " ++ show result
+                        -- Clean up
+                        res_unmap_wmemPtr <- munmap wmemPtr (fromIntegral jitMemoryCap)
+                        if res_unmap_wmemPtr /= 0
+                            then putStrLn "Failed to unmap working memory"
+                            else do 
+                                putStrLn "Working memory unmapped successfully"
+                                result_unmap <- munmap codeMPtr (fromIntegral memSize)
+                                if result_unmap /= 0
+                                    then putStrLn "Failed to unmap code memory"
+                                    else putStrLn "Code memory unmapped successfully"
 
 -- | C function to call the executable memory
 foreign import ccall unsafe "dynamic"
